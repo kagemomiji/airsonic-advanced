4dca8bd7961488221d950b59305dd9fa41809276
Y.Tory <5343692+kagemomiji@users.noreply.github.com>
2023-03-31 15:22:52 +0000

    #161 test strip HTML

--- airsonic-main/src/main/webapp/script/DataTables/Buttons-2.3.6/js/buttons.colVis.js
+++ airsonic-main/src/main/webapp/script/DataTables/Buttons-2.3.6/js/buttons.colVis.js
@@ -199,9 +199,7 @@ $.extend( DataTable.ext.buttons, {
 				.replace(/<br\s*\/?>/gi, " ")  // replace line breaks with spaces
 				.replace(/<select(.*?)<\/select>/g, "") // remove select tags, including options text
 				.replace(/<!--[\s\S]*?-->/gi, "") // strip HTML comments
-				.replace(/<[^>]*>/g, "")   // strip HTML
-				.replace(/>/g, "&gt;")
-				.replace(/</g, "&lt;")
+				.replace(/<\/?[^>]+(>|$)/g, "")   // strip HTML
 				.replace(/^\s+|\s+$/g,""); // trim
 
 			return conf.columnText ?
